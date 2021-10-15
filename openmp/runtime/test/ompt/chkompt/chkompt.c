4f4088946e3a6473aff3f62b19df71706e965f0a
vladaindjic <vladaindjic@icloud.com>
2021-10-15 12:49:42 +0200

    chkompt reformatted

--- openmp/runtime/test/ompt/chkompt/chkompt.c
+++ openmp/runtime/test/ompt/chkompt/chkompt.c
@@ -10,83 +10,83 @@
 
 #include "chkompt.h"
 
-int	nfails = 0;
+int nfails = 0;
 
 void (*skew_delay_ptr)(int) = skew_delay;
+
 void (*delay_ptr)(int) = delay;
 
 // ------------------------------------------------------------------------
 // Main program -- drive various tests
 // ------------------------------------------------------------------------
 
-int main(int argc, char **argv)
-{
-	char	buf[256];
+int main(int argc, char **argv) {
+  char buf[256];
 
-	starttime = gethrtime();
-	// fprintf(stderr, "main invoked\n");
+  starttime = gethrtime();
+  // fprintf(stderr, "main invoked\n");
 
-	// Set thread count; causes the initialization of the OMPT code
-	omp_set_num_threads(NUMTHREADS);
+  // Set thread count; causes the initialization of the OMPT code
+  omp_set_num_threads(NUMTHREADS);
 
 #if 0
-	// test lock callbacks
-	lockcbtest();
-	(*delay_ptr)(10);
+  // test lock callbacks
+  lockcbtest();
+  (*delay_ptr)(10);
 
-	// test reduction
-	reductiontest();
-	(*delay_ptr)(10);
+  // test reduction
+  reductiontest();
+  (*delay_ptr)(10);
 #endif
 
-	// Test frames for multiple loops in a single parallel region
-	testparallel();
-	(*delay_ptr)(10);
+  // Test frames for multiple loops in a single parallel region
+  testparallel();
+  (*delay_ptr)(10);
 
-	// Test frames for independent parallel for loops with static scheduling
-	testparallelfor();
-	(*delay_ptr)(10);
+  // Test frames for independent parallel for loops with static scheduling
+  testparallelfor();
+  (*delay_ptr)(10);
 
 #if 0
 
-	// test parallel sections
-	testparallelsections();
-	(*delay_ptr)(10);
+  // test parallel sections
+  testparallelsections();
+  (*delay_ptr)(10);
 
-	// test explicit tasks
-	testtasks();
-	(*delay_ptr)(10);
+  // test explicit tasks
+  testtasks();
+  (*delay_ptr)(10);
 
-	// test triply nested loops
-	testtriple_nest();
-	(*delay_ptr)(10);
+  // test triply nested loops
+  testtriple_nest();
+  (*delay_ptr)(10);
 #ifndef NO_NONEST
-	testtriple_nonest();
-	(*delay_ptr)(10);
+  testtriple_nonest();
+  (*delay_ptr)(10);
 #endif
 
 #endif
 
-	// Check for failures
-	if (nfails != 0 ) {
-	    sprintf(buf, "\n       FAILURE:\n\t%d ERROR%s detected\n\n",
-		nfails,
-		nfails == 1 ? "" : "s" );
-	    ts_write(buf);
-	    printf("\n       FAILURE:\n\t%d ERROR%s detected\n\n",
-		nfails,
-		nfails == 1 ? "" : "s" );
-		
-	    exit(1);
-	} else {
-	    ts_write("\n       No failures\n\n");
-	    printf("No failures\n");
-	    exit(0);
-	}
+  // Check for failures
+  if (nfails != 0) {
+    sprintf(buf, "\n       FAILURE:\n\t%d ERROR%s detected\n\n",
+            nfails,
+            nfails == 1 ? "" : "s");
+    ts_write(buf);
+    printf("\n       FAILURE:\n\t%d ERROR%s detected\n\n",
+           nfails,
+           nfails == 1 ? "" : "s");
+
+    exit(1);
+  } else {
+    ts_write("\n       No failures\n\n");
+    printf("No failures\n");
+    exit(0);
+  }
 // CHECK: No failures
 }
 
-void foo(){
+void foo() {
   volatile float x;
   for (int i = 0; i < 100; i++) {
     x = 0.0;
@@ -99,215 +99,207 @@ void foo(){
 // ------------------------------------------------------------------------
 // Test "omp parallel" with "omp for" loops with various schedules
 // ------------------------------------------------------------------------
-void testparallel()
-{
-	int i;
-	ts_write("\n                  starting testparallel\n\n");
+void testparallel() {
+  int i;
+  ts_write("\n                  starting testparallel\n\n");
 #pragma omp parallel private(i)
-	{
+  {
     (*vi3_validate_ptr)("parallel begin", APP);
 
 
 #pragma omp master
-	ts_write("\n                  starting for\n\n");
+    ts_write("\n                  starting for\n\n");
 #pragma omp for
-		    for(i = 0; i < N; i++) (*vi3_validate_ptr)("for", APP);
+    for (i = 0; i < N; i++) (*vi3_validate_ptr)("for", APP);
 
 #pragma omp master
-	    (*delay_ptr)(10);
+    (*delay_ptr)(10);
 
 #pragma omp master
-	ts_write("\n                  starting for static\n\n");
+    ts_write("\n                  starting for static\n\n");
 #pragma omp for schedule(static)
-	    for(i = 0; i < N; i++) (*vi3_validate_ptr)("for schedule(static)", APP);
+    for (i = 0; i < N; i++) (*vi3_validate_ptr)("for schedule(static)", APP);
 
 #pragma omp master
-	    (*delay_ptr)(10);
+    (*delay_ptr)(10);
 
 #pragma omp master
-	ts_write("\n                  starting for dynamic\n\n");
+    ts_write("\n                  starting for dynamic\n\n");
 #pragma omp for schedule(dynamic)
-	    for(i = 0; i < N; i++) (*vi3_validate_ptr)("for schedule(dynamic)", APP);
+    for (i = 0; i < N; i++) (*vi3_validate_ptr)("for schedule(dynamic)", APP);
 
 #pragma omp master
-	    (*delay_ptr)(10);
+    (*delay_ptr)(10);
 
 #pragma omp master
-	ts_write("\n                  starting for guided\n\n");
+    ts_write("\n                  starting for guided\n\n");
 #pragma omp for schedule(guided)
-	    for(i = 0; i < N; i++) (*vi3_validate_ptr)("for schedule(guided)", APP);
+    for (i = 0; i < N; i++) (*vi3_validate_ptr)("for schedule(guided)", APP);
 
 #pragma omp master
-	    (*delay_ptr)(10);
+    (*delay_ptr)(10);
 
-	    (*vi3_validate_ptr)("parallel end", APP);
-	}
+    (*vi3_validate_ptr)("parallel end", APP);
+  }
 }
 
 // ------------------------------------------------------------------------
 // Test "omp parallel for" loops with various schedules
 // ------------------------------------------------------------------------
 void
-testparallelfor()
-{
-	teststatic();
-	(*delay_ptr)(10);
+testparallelfor() {
+  teststatic();
+  (*delay_ptr)(10);
 
-	testdynamic();
-	(*delay_ptr)(10);
+  testdynamic();
+  (*delay_ptr)(10);
 
-	testguided();
-	(*delay_ptr)(10);
+  testguided();
+  (*delay_ptr)(10);
 #if 0
-	testsections();
-	(*delay_ptr)(10);
+  testsections();
+  (*delay_ptr)(10);
 #endif
 }
 
 // ------------------------------------------------------------------------
 // Test "omp parallel for" loops with various schedules
 // ------------------------------------------------------------------------
-void teststatic()
-{
-	int i;
-	ts_write("\n                  starting teststatic\n\n");
+void teststatic() {
+  int i;
+  ts_write("\n                  starting teststatic\n\n");
 #pragma omp parallel for schedule(static) private(i)
-	for(i = 0; i < N; i++) (*vi3_validate_ptr)("parallel for static", APP);
+  for (i = 0; i < N; i++) (*vi3_validate_ptr)("parallel for static", APP);
 }
 
 
-void testdynamic()
-{
-	int i;
-	ts_write("\n                  starting testdynamic\n\n");
+void testdynamic() {
+  int i;
+  ts_write("\n                  starting testdynamic\n\n");
 #pragma omp parallel for schedule(dynamic) private(i)
-	for(i = 0; i < N; i++) (*vi3_validate_ptr)("parallel for dynamic", APP);
+  for (i = 0; i < N; i++) (*vi3_validate_ptr)("parallel for dynamic", APP);
 }
 
 
-void testguided()
-{
-	int i;
-	ts_write("\n                  starting testguided\n\n");
+void testguided() {
+  int i;
+  ts_write("\n                  starting testguided\n\n");
 #pragma omp parallel for schedule(guided) private(i)
-	for(i = 0; i < N; i++) (*vi3_validate_ptr)("parallel for guided", APP);
+  for (i = 0; i < N; i++) (*vi3_validate_ptr)("parallel for guided", APP);
 }
 
 // ------------------------------------------------------------------------
 // Test "omp sections"
 // ------------------------------------------------------------------------
-void testsections()
-{
-	    ts_write("\n                  starting testsections\n\n");
+void testsections() {
+  ts_write("\n                  starting testsections\n\n");
 #pragma omp parallel
-	{
+  {
 #pragma omp sections
-	    {
+    {
 #pragma omp section
-		{
-		    (*validate_ptr)("omp section 1");
-#ifdef	RUN_SKEW
-		    (*skew_delay_ptr)(1);
+      {
+        (*validate_ptr)("omp section 1");
+#ifdef  RUN_SKEW
+        (*skew_delay_ptr)(1);
 #endif
-		}
+      }
 #pragma omp section
-		{
-		    (*validate_ptr)("omp section 2");
-#ifdef	RUN_SKEW
-		    (*skew_delay_ptr)(2);
+      {
+        (*validate_ptr)("omp section 2");
+#ifdef  RUN_SKEW
+        (*skew_delay_ptr)(2);
 #endif
-		}
+      }
 #pragma omp section
-		{
-		    (*validate_ptr)("omp section 3");
-#ifdef	RUN_SKEW
-		    (*skew_delay_ptr)(3);
+      {
+        (*validate_ptr)("omp section 3");
+#ifdef  RUN_SKEW
+        (*skew_delay_ptr)(3);
 #endif
-		}
-	    }
-	}
+      }
+    }
+  }
 }
 
 
-void testparallelsections()
-{
-	    ts_write("\n                  starting testparallelsections\n\n");
+void testparallelsections() {
+  ts_write("\n                  starting testparallelsections\n\n");
 #pragma omp parallel sections num_threads(NUMTHREADS)
-	{
+  {
 #pragma omp section
-	      (*validate_ptr)("omp parallel section 1");
+    (*validate_ptr)("omp parallel section 1");
 #pragma omp section
-	      (*validate_ptr)("omp parallel section 2");
+    (*validate_ptr)("omp parallel section 2");
 #pragma omp section
-	      (*validate_ptr)("omp parallel section 3");
-	}
+    (*validate_ptr)("omp parallel section 3");
+  }
 }
 
-void testtasks()
-{
-	    ts_write("\n                  starting testtasks\n\n");
+void testtasks() {
+  ts_write("\n                  starting testtasks\n\n");
 #pragma omp parallel
-	{
+  {
 #pragma omp single
-	    {
+    {
 #pragma omp task
-	      (*validate_ptr)("omp task 1");
+      (*validate_ptr)("omp task 1");
 #pragma omp task
-	      (*validate_ptr)("omp task 2");
+      (*validate_ptr)("omp task 2");
 #pragma omp task
-	      (*validate_ptr)("omp task 3");
+      (*validate_ptr)("omp task 3");
 #pragma omp task
-	      (*validate_ptr)("omp task 4");
+      (*validate_ptr)("omp task 4");
 #pragma omp task
-	      (*validate_ptr)("omp task 5");
+      (*validate_ptr)("omp task 5");
 #pragma omp task
-	      (*validate_ptr)("omp task 6");
+      (*validate_ptr)("omp task 6");
 #pragma omp task
-	      (*validate_ptr)("omp task 7");
+      (*validate_ptr)("omp task 7");
 #pragma omp task
-	      (*validate_ptr)("omp task 8");
+      (*validate_ptr)("omp task 8");
 #pragma omp task
-	      (*validate_ptr)("omp task 9");
-	    }
-	}
+      (*validate_ptr)("omp task 9");
+    }
+  }
 }
 
 
-void	loop0();
-void	loop1();
-void	loop2();
-void	loop3();
+void loop0();
+
+void loop1();
+
+void loop2();
+
+void loop3();
 
 // testtriple_nest -- test a triply-nested set of loops, with nesting enabled
 //
-void testtriple_nest()
-{
-	ts_write("\n                  starting testtriple_nest\n\n");
+void testtriple_nest() {
+  ts_write("\n                  starting testtriple_nest\n\n");
 
-	// Set omp_max_active_levels, to allow nested loops
-	omp_set_max_active_levels(5);
+  // Set omp_max_active_levels, to allow nested loops
+  omp_set_max_active_levels(5);
 
-	// now invoke the triply-nested loop
-	testtriple();
+  // now invoke the triply-nested loop
+  testtriple();
 }
 
 // testtriple_nonest -- test a triply-nested set of loops, with nesting disabled
 //
-void testtriple_nonest()
-{
-	ts_write("\n                  starting testtriple_nonest\n\n");
+void testtriple_nonest() {
+  ts_write("\n                  starting testtriple_nonest\n\n");
 
-	// Set omp_max_active_levels, to 1, disallowing nesting
-	omp_set_max_active_levels(1);
+  // Set omp_max_active_levels, to 1, disallowing nesting
+  omp_set_max_active_levels(1);
 
-	// now invoke the triply-nested loop
-	testtriple();
+  // now invoke the triply-nested loop
+  testtriple();
 }
 
 // testtriple --the actual code, triply nested in source
 void
-testtriple()
-{
+testtriple() {
 #pragma omp parallel num_threads(2)
   {
     loop0();
@@ -316,11 +308,11 @@ testtriple()
       loop1();
 #pragma omp parallel num_threads(3)
       {
-	loop2();
+        loop2();
 #pragma omp parallel num_threads(3)
-	{
-	  loop3();
-	}
+        {
+          loop3();
+        }
       }
     }
   }
@@ -330,87 +322,81 @@ testtriple()
 #define ITERATIONS 100000000
 
 void
-form_label(char *buffer, char *label)
-{
-	int level, thread, pthread;
+form_label(char *buffer, char *label) {
+  int level, thread, pthread;
 
-	level = omp_get_level();
-	thread = omp_get_thread_num();
-	pthread = omp_get_ancestor_thread_num(level);
-	sprintf(buffer, "Begin %s t=%d l=%d pt=%d", label, thread, level, pthread);
+  level = omp_get_level();
+  thread = omp_get_thread_num();
+  pthread = omp_get_ancestor_thread_num(level);
+  sprintf(buffer, "Begin %s t=%d l=%d pt=%d", label, thread, level, pthread);
 }
 
 void
-loop0()
-{
-	int j;
-	char buf[100];
-	form_label( buf, "loop0");
+loop0() {
+  int j;
+  char buf[100];
+  form_label(buf, "loop0");
 
-	(*validate_ptr)(buf);
-	for(j=0;j<ITERATIONS;j+=2) j--;
+  (*validate_ptr)(buf);
+  for (j = 0; j < ITERATIONS; j += 2) j--;
 
-	strncpy (buf, "End   ", 6);
-	(*validate_ptr)(buf);
+  strncpy(buf, "End   ", 6);
+  (*validate_ptr)(buf);
 }
 
 void
-loop1()
-{
-	int j;
-	char buf[100];
-	form_label( buf, "loop1");
+loop1() {
+  int j;
+  char buf[100];
+  form_label(buf, "loop1");
 
-	(*validate_ptr)(buf);
-	for(j=0;j<ITERATIONS;j+=2) j--;
+  (*validate_ptr)(buf);
+  for (j = 0; j < ITERATIONS; j += 2) j--;
 
-	strncpy (buf, "End   ", 6);
-	(*validate_ptr)(buf);
+  strncpy(buf, "End   ", 6);
+  (*validate_ptr)(buf);
 }
 
 void
-loop2()
-{
-	int j;
-	char buf[100];
-	form_label( buf, "loop2");
+loop2() {
+  int j;
+  char buf[100];
+  form_label(buf, "loop2");
 
-	(*validate_ptr)(buf);
-	for(j=0;j<ITERATIONS;j+=2) j--;
+  (*validate_ptr)(buf);
+  for (j = 0; j < ITERATIONS; j += 2) j--;
 
-	strncpy (buf, "End   ", 6);
-	(*validate_ptr)(buf);
+  strncpy(buf, "End   ", 6);
+  (*validate_ptr)(buf);
 }
 
 void
-loop3()
-{
-	int j;
-	char buf[100];
-	form_label( buf, "loop3");
+loop3() {
+  int j;
+  char buf[100];
+  form_label(buf, "loop3");
 
-	(*validate_ptr)(buf);
-	for(j=0;j<ITERATIONS;j+=2) j--;
+  (*validate_ptr)(buf);
+  for (j = 0; j < ITERATIONS; j += 2) j--;
 
-	strncpy (buf, "End   ", 6);
-	(*validate_ptr)(buf);
+  strncpy(buf, "End   ", 6);
+  (*validate_ptr)(buf);
 }
 
 // reductiontest -- check for appropriate callbacks
 //
 void
-reductiontest()
-{
-	int sum, i;
+reductiontest() {
+  int sum, i;
 
-	ts_write("\n                  starting reductiontest\n\n");
-	sum = 0;
+  ts_write("\n                  starting reductiontest\n\n");
+  sum = 0;
 
 #pragma omp parallel for reduction(+:sum)
-	for(i = 0; i < N; i++) {
-	    sum += i;
-	    (*validate_ptr)("reductiontest");
-	}
+  for (i = 0; i < N; i++) {
+    sum += i;
+    (*validate_ptr)("reductiontest");
+  }
 }
 
 // -----------------------------------------------------------
@@ -418,84 +404,83 @@ reductiontest()
 // the code pointers are plausible
 //
 void
-lockcbtest()
-{
-	omp_lock_t	lock1, lock2;
-	omp_nest_lock_t	lock3;
+lockcbtest() {
+  omp_lock_t lock1, lock2;
+  omp_nest_lock_t lock3;
 
-	ts_write("\n                  starting lockcbtest\n\n");
+  ts_write("\n                  starting lockcbtest\n\n");
 
-	// initialize the locks
-	omp_init_lock(&lock1);
-	omp_init_lock(&lock2);
-	omp_init_nest_lock(&lock3);
+  // initialize the locks
+  omp_init_lock(&lock1);
+  omp_init_lock(&lock2);
+  omp_init_nest_lock(&lock3);
 
 #pragma omp parallel
-	{
-	    (*validate_ptr)("lockcb start");
+  {
+    (*validate_ptr)("lockcb start");
 #pragma omp master
-	    {
-	omp_set_lock(&lock1); 	// code pointer should be approximately label1
-label1:	omp_unset_lock(&lock1);
+    {
+      omp_set_lock(&lock1);  // code pointer should be approximately label1
+      label1:
+      omp_unset_lock(&lock1);
 
-	omp_set_lock(&lock2); 	// code pointer should be approximately label2
-label2:	omp_unset_lock(&lock2);
+      omp_set_lock(&lock2);  // code pointer should be approximately label2
+      label2:
+      omp_unset_lock(&lock2);
 
-	// now try a nested lock
-	omp_set_nest_lock(&lock3);
-	omp_set_nest_lock(&lock3);
-	omp_set_nest_lock(&lock3);
+      // now try a nested lock
+      omp_set_nest_lock(&lock3);
+      omp_set_nest_lock(&lock3);
+      omp_set_nest_lock(&lock3);
 
-	omp_unset_nest_lock(&lock3);
-	omp_unset_nest_lock(&lock3);
-	omp_unset_nest_lock(&lock3);
-	    }
+      omp_unset_nest_lock(&lock3);
+      omp_unset_nest_lock(&lock3);
+      omp_unset_nest_lock(&lock3);
+    }
 
-	    (*validate_ptr)("lockcb end");
-	}
-	omp_destroy_lock(&lock1);
-	omp_destroy_lock(&lock2);
-	omp_destroy_nest_lock(&lock3);
+    (*validate_ptr)("lockcb end");
+  }
+  omp_destroy_lock(&lock1);
+  omp_destroy_lock(&lock2);
+  omp_destroy_nest_lock(&lock3);
 }
 
 // ------------------------------------------------------------------------
 // skew_delay -- burn CPU time to delay threads
 // ------------------------------------------------------------------------
 void
-skew_delay(int count)
-{
-	int j,k;
-	volatile float x;
-	int	jmax;
+skew_delay(int count) {
+  int j, k;
+  volatile float x;
+  int jmax;
 
-	jmax = 7 * count;
+  jmax = 7 * count;
 
-	for ( j = 0; j < jmax; j++ ) {
-	    x = 0;
-	    for (k = 0; k < NSKEW; k ++ ) {
-		x = x + 1.0;
-	    }
-	}
+  for (j = 0; j < jmax; j++) {
+    x = 0;
+    for (k = 0; k < NSKEW; k++) {
+      x = x + 1.0;
+    }
+  }
 }
 
 // ------------------------------------------------------------------------
 // delay -- burn CPU time in main program to space out operations
 // ------------------------------------------------------------------------
 void
-delay(int count)
-{
-	int j,k;
-	volatile float x;
-	int	jmax;
+delay(int count) {
+  int j, k;
+  volatile float x;
+  int jmax;
 
-	jmax = 7 * count;
+  jmax = 7 * count;
 
-	for ( j = 0; j < jmax; j++ ) {
-	    x = 0;
-	    for (k = 0; k < NSKEW; k ++ ) {
-		x = x + 1.0;
-	    }
-	}
+  for (j = 0; j < jmax; j++) {
+    x = 0;
+    for (k = 0; k < NSKEW; k++) {
+      x = x + 1.0;
+    }
+  }
 }
 
 #include "omptcb.h"
--- openmp/runtime/test/ompt/chkompt/chkompt.h
+++ openmp/runtime/test/ompt/chkompt/chkompt.h
@@ -25,101 +25,156 @@
 // ------------------------------------------------------------------------
 // Subtests for various parts of the OMPT interface
 // ------------------------------------------------------------------------
-void	testparallel();
-void	testparallelfor();
-void	teststatic();
-void	testdynamic();
-void	testguided();
-void	testsections();
-void	testparallelsections();
-void	testtasks();
-void	testtriple_nest();
-void	testtriple_nonest();
-void	testtriple();
-void	reductiontest();
-void	lockcbtest();
+void testparallel();
+
+void testparallelfor();
+
+void teststatic();
+
+void testdynamic();
+
+void testguided();
+
+void testsections();
+
+void testparallelsections();
+
+void testtasks();
+
+void testtriple_nest();
+
+void testtriple_nonest();
+
+void testtriple();
+
+void reductiontest();
+
+void lockcbtest();
 
 // Utility routines to validate the data and delay threads
-void	skew_delay(int);
-void	format_sync_type(ompt_sync_region_t, ompt_scope_endpoint_t endpoint, char *);
-void	format_work_type(ompt_work_t, ompt_scope_endpoint_t endpoint, char *);
-void	format_task_type(int, char *);
-void	format_lock_type(ompt_mutex_t, char *);
-void	ck_ra(const char *, int, const void*, int, char*);
-void	validate(const char *);
+void skew_delay(int);
+
+void format_sync_type(ompt_sync_region_t, ompt_scope_endpoint_t endpoint, char *);
+
+void format_work_type(ompt_work_t, ompt_scope_endpoint_t endpoint, char *);
+
+void format_task_type(int, char *);
+
+void format_lock_type(ompt_mutex_t, char *);
+
+void ck_ra(const char *, int, const void *, int, char *);
+
+void validate(const char *);
+
 void validate_task_frame_hierarchy(const char *, char);
-void	delay(int);
+
+void delay(int);
 
 // pointers to those routines
-void	(*skew_delay_ptr)(int);
-void	(*ck_ra_ptr)(const char *, int, const void *, char *);
-void	(*validate_ptr)(const char *);
-void	(*vi3_validate_ptr)(const char *, char);
+void (*skew_delay_ptr)(int);
 
-void	(*delay_ptr)(int);
+void (*ck_ra_ptr)(const char *, int, const void *, char *);
+
+void (*validate_ptr)(const char *);
+
+void (*vi3_validate_ptr)(const char *, char);
+
+void (*delay_ptr)(int);
 
 // OpenMP library invocation routines
 ompt_function_lookup_t my_lookup;
-ompt_start_tool_result_t * ompt_start_tool(unsigned int, const char *);
-void	ompt_finalize();
-int	ompt_initialize(ompt_function_lookup_t, int, ompt_data_t *);
-void	register_callbacks();
+
+ompt_start_tool_result_t *ompt_start_tool(unsigned int, const char *);
+
+void ompt_finalize();
+
+int ompt_initialize(ompt_function_lookup_t, int, ompt_data_t *);
+
+void register_callbacks();
 
 // OpenMP library functions, looked up at initialization
 ompt_set_callback_t ompt_set_callback_fn;
 ompt_get_task_info_t ompt_get_task_info_fn;
 
 // OpenMP callback routines
-void	ompt_implicit_task(ompt_scope_endpoint_t, ompt_data_t*, ompt_data_t*, unsigned int, unsigned int, int);
-void	ompt_thread_begin(ompt_thread_t, ompt_data_t *);
-void	ompt_thread_end(ompt_data_t *);
-void	ompt_parallel_begin(ompt_data_t *, const ompt_frame_t *, ompt_data_t *, unsigned int, int, const void *);
-void	ompt_parallel_end(ompt_data_t *, ompt_data_t *, int, const void *);
-void	ompt_task_create(ompt_data_t *, const ompt_frame_t *, ompt_data_t *, int, int, const void *);
-void	ompt_task_schedule(ompt_data_t *, ompt_task_status_t, ompt_data_t *);
-void	ompt_implicit_task(ompt_scope_endpoint_t, ompt_data_t*, ompt_data_t*, unsigned int, unsigned int, int);
+void ompt_implicit_task(ompt_scope_endpoint_t, ompt_data_t *, ompt_data_t *, unsigned int, unsigned int, int);
 
-void	ompt_targetcb (ompt_target_t, ompt_scope_endpoint_t, int, ompt_id_t, ompt_id_t, const void *);
+void ompt_thread_begin(ompt_thread_t, ompt_data_t *);
 
-void	ompt_target_data_op( ompt_id_t, ompt_id_t, ompt_target_data_op_t, void *, int, void *, int, size_t, const void *);
-void	ompt_target_submit (ompt_id_t, ompt_id_t, unsigned int);
+void ompt_thread_end(ompt_data_t *);
 
-void	ompt_control_tool(uint64_t, uint64_t, void *, const void *);
-void	ompt_device_initialize(int, const char*, ompt_device_t *, ompt_function_lookup_t, const char *);
-void	ompt_device_finalize(int);
-void	ompt_device_load(int, const char*, int64_t, void*, size_t, void*, void*,uint64_t);
-void	ompt_device_unload(int, uint64_t);
+void ompt_parallel_begin(ompt_data_t *, const ompt_frame_t *, ompt_data_t *, unsigned int, int, const void *);
 
-void	ompt_sync_region_wait(ompt_sync_region_t, ompt_scope_endpoint_t, ompt_data_t *, ompt_data_t *, const void *);
-void	ompt_mutex_released(ompt_mutex_t, ompt_wait_id_t, const void *);
+void ompt_parallel_end(ompt_data_t *, ompt_data_t *, int, const void *);
 
-void	ompt_dependences(ompt_data_t*, const ompt_dependence_t *, int);
-void	ompt_task_dependence(ompt_data_t*, ompt_data_t *);
+void ompt_task_create(ompt_data_t *, const ompt_frame_t *, ompt_data_t *, int, int, const void *);
 
-void	ompt_work(ompt_work_t, ompt_scope_endpoint_t, ompt_data_t *, ompt_data_t *, uint64_t, const void *);
-void	ompt_master( ompt_scope_endpoint_t, ompt_data_t *, ompt_data_t *, const void *);
-void	ompt_target_map(ompt_id_t, unsigned int, void **, void **, size_t*, unsigned int *, const void *);
-void	ompt_sync_region(ompt_sync_region_t, ompt_scope_endpoint_t, ompt_data_t *, ompt_data_t *, const void *);
-void	ompt_lock_init( ompt_mutex_t, unsigned int, unsigned int, ompt_wait_id_t, const void *);
-void	ompt_lock_destroy(ompt_mutex_t, ompt_wait_id_t, const void *);
-void	ompt_mutex_acquire( ompt_mutex_t, unsigned int, unsigned int, ompt_wait_id_t, const void *);
-void	ompt_mutex_acquired(ompt_mutex_t, ompt_wait_id_t, const void *);
-void	ompt_nest_lock(ompt_scope_endpoint_t, ompt_wait_id_t, const void *);
-void	ompt_flush(ompt_data_t *, const void *);
-void	ompt_cancel(ompt_data_t *, int, const void *);
-void	ompt_reduction();
-void	ompt_dispatch(ompt_data_t *, ompt_data_t *, ompt_dispatch_t, ompt_data_t instance);
+void ompt_task_schedule(ompt_data_t *, ompt_task_status_t, ompt_data_t *);
+
+void ompt_implicit_task(ompt_scope_endpoint_t, ompt_data_t *, ompt_data_t *, unsigned int, unsigned int, int);
+
+void ompt_targetcb(ompt_target_t, ompt_scope_endpoint_t, int, ompt_id_t, ompt_id_t, const void *);
+
+void ompt_target_data_op(ompt_id_t, ompt_id_t, ompt_target_data_op_t, void *, int, void *, int, size_t, const void *);
+
+void ompt_target_submit(ompt_id_t, ompt_id_t, unsigned int);
+
+void ompt_control_tool(uint64_t, uint64_t, void *, const void *);
+
+void ompt_device_initialize(int, const char *, ompt_device_t *, ompt_function_lookup_t, const char *);
+
+void ompt_device_finalize(int);
+
+void ompt_device_load(int, const char *, int64_t, void *, size_t, void *, void *, uint64_t);
+
+void ompt_device_unload(int, uint64_t);
+
+void ompt_sync_region_wait(ompt_sync_region_t, ompt_scope_endpoint_t, ompt_data_t *, ompt_data_t *, const void *);
+
+void ompt_mutex_released(ompt_mutex_t, ompt_wait_id_t, const void *);
+
+void ompt_dependences(ompt_data_t *, const ompt_dependence_t *, int);
+
+void ompt_task_dependence(ompt_data_t *, ompt_data_t *);
+
+void ompt_work(ompt_work_t, ompt_scope_endpoint_t, ompt_data_t *, ompt_data_t *, uint64_t, const void *);
+
+void ompt_master(ompt_scope_endpoint_t, ompt_data_t *, ompt_data_t *, const void *);
+
+void ompt_target_map(ompt_id_t, unsigned int, void **, void **, size_t *, unsigned int *, const void *);
+
+void ompt_sync_region(ompt_sync_region_t, ompt_scope_endpoint_t, ompt_data_t *, ompt_data_t *, const void *);
+
+void ompt_lock_init(ompt_mutex_t, unsigned int, unsigned int, ompt_wait_id_t, const void *);
+
+void ompt_lock_destroy(ompt_mutex_t, ompt_wait_id_t, const void *);
+
+void ompt_mutex_acquire(ompt_mutex_t, unsigned int, unsigned int, ompt_wait_id_t, const void *);
+
+void ompt_mutex_acquired(ompt_mutex_t, ompt_wait_id_t, const void *);
+
+void ompt_nest_lock(ompt_scope_endpoint_t, ompt_wait_id_t, const void *);
+
+void ompt_flush(ompt_data_t *, const void *);
+
+void ompt_cancel(ompt_data_t *, int, const void *);
+
+void ompt_reduction();
+
+void ompt_dispatch(ompt_data_t *, ompt_data_t *, ompt_dispatch_t, ompt_data_t instance);
 
 // Timing routines
-typedef long long      hrtime_t;        // time in nanoseconds
-hrtime_t	gethrtime();
-hrtime_t	gethrvtime();
-hrtime_t	gethrustime();
+typedef long long hrtime_t;        // time in nanoseconds
+hrtime_t gethrtime();
 
-void	ts_write(char *);
+hrtime_t gethrvtime();
+
+hrtime_t gethrustime();
+
+void ts_write(char *);
 
 // Data -- keeping track of failures
-int     nfails;
+int nfails;
 
-hrtime_t	starttime;
+hrtime_t starttime;
 
